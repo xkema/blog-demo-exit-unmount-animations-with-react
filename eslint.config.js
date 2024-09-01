75d9b3263640f8cd564b94f7a819fe2d8f4ad537
Kemal Yılmaz <xkema@tutanota.com>
2024-09-01 12:20:15 +0300

    Set up the `@stylistic/eslint-plugin` for JSX formatting
    - Overrides the Vite `eslint` rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@stylistic': stylistic,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +35,7 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      ...stylistic.configs['recommended-flat'].rules,
     },
   },
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Exit/Unmount Animations with React - Blog Demo</h1>
       <a href="/">Exit/Unmount Animations with React - Blog Demo</a>
     </div>
