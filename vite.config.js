321450f574421c5adc07c9f4dd52dc1738923f7c
Kemal Yılmaz <xkema@tutanota.com>
2024-09-06 22:24:11 +0300

    Updated Vite `outDir` and `base` options for GitHub Pages deployment
    - Added `.nojekyll`

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/blog-demo-exit-unmount-animations-with-react/',
+  build: {
+    outDir: 'docs',
+  },
 })
